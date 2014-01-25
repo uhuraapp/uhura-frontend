7c9321a14ed50c67ab9781a1967d9ec82cc9301f
Duke <duke@riseup.net>
2014-01-25 17:31:20 -0200

    only subscribed user if channel has Title

--- core/feed.go
+++ core/feed.go
@@ -6,11 +6,16 @@ func AddFeed(feed_url string, userId int) ChannelResult {
 	var channel Channel
 	var userChannel UserChannel
 	database.FirstOrCreate(&channel, map[string]interface{}{"url": feed_url})
-	go database.Where(UserChannel{ChannelId: channel.Id, UserId: userId}).FirstOrCreate(&userChannel)
 
 	FetchChanell(channel)
 	<-ChannelChan
+
 	var result ChannelResult
 	database.Table("channels").First(&result, channel.Id)
+	if result.Title != "" {
+		go database.Where(UserChannel{ChannelId: channel.Id, UserId: userId}).FirstOrCreate(&userChannel)
+		result.Subscribed = true
+	}
+
 	return result
 }
