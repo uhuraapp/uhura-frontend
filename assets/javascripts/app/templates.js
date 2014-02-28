2ad7c7ddc45db5648582436c0b962e7dcb309b02
Duke <duke@riseup.net>
2014-02-27 23:46:11 -0300

    build assets

--- assets/javascripts/app/templates.js
+++ assets/javascripts/app/templates.js
@@ -6,42 +6,42 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   
-  data.buffer.push("\n        <span class=\"typcn typcn-home-outline\"></span>Home\n      ");
+  data.buffer.push("\n          <span class=\"typcn typcn-home-outline\"></span>Home\n        ");
   }
 
 function program3(depth0,data) {
   
   var buffer = '', stack1, helper, options;
-  data.buffer.push("\n    <li>\n      ");
+  data.buffer.push("\n      <li>\n        ");
   stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0,depth0],types:["STRING","ID"],data:data},helper ? helper.call(depth0, "channel", "channel.id", options) : helperMissing.call(depth0, "link-to", "channel", "channel.id", options));
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n    </li>\n  ");
+  data.buffer.push("\n      </li>\n    ");
   return buffer;
   }
 function program4(depth0,data) {
   
   var buffer = '', stack1;
-  data.buffer.push("\n        <img ");
+  data.buffer.push("\n          <img ");
   data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
     'src': ("channel.image_url")
   },hashTypes:{'src': "ID"},hashContexts:{'src': depth0},contexts:[],types:[],data:data})));
-  data.buffer.push(" />\n        <span class=\"title\">");
+  data.buffer.push(" />\n          <span class=\"title\">");
   stack1 = helpers._triageMustache.call(depth0, "channel.title", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("</span>\n        <i>");
+  data.buffer.push("</span>\n          <i>");
   stack1 = helpers._triageMustache.call(depth0, "channel.to_view", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("</i>\n      ");
+  data.buffer.push("</i>\n        ");
   return buffer;
   }
 
-  data.buffer.push("<div id=\"side-left\" class=\"column three-twelfth\">\n  <header>\n    <hgroup>\n      <h1>Uhura</h1>\n      <h2>Podcast Manager</h2>\n    </hgroup>\n  </header>\n  <ul>\n    <li>\n      ");
+  data.buffer.push("<div id=\"aside-wrapper\" class=\"column three-twelfth\">\n  <aside class=\"column three-twelfth\">\n    <header>\n      <hgroup>\n        <h1>Uhura</h1>\n        <h2>Podcast Manager</h2>\n      </hgroup>\n    </header>\n\n    <ul>\n      <li>\n        ");
   stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],data:data},helper ? helper.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n    </li>\n    <li><a href=\"/app/\"><span class=\"typcn typcn-document-add\"></span>Add Channel</a></li>\n  </ul>\n  <ol>\n  ");
+  data.buffer.push("\n      </li>\n      <li><a href=\"/app/\"><span class=\"typcn typcn-document-add\"></span>Add Channel</a></li>\n    </ul>\n    <ol>\n    ");
   stack1 = helpers.each.call(depth0, "channel", "in", "channels", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n  </ol>\n\n  <footer>\n  <a>Follow us <span class=\"typcn typcn-social-twitter\"></span></a>\n  </footer>\n</div>\n\n\n\n<div id=\"content\" class=\"column nine-twelfth\">\n  ");
+  data.buffer.push("\n    </ol>\n\n    <footer>\n    <a>Follow us <span class=\"typcn typcn-social-twitter\"></span></a>\n    </footer>\n  </aside>\n</div>\n\n\n\n<div id=\"content\" class=\"column nine-twelfth\">\n  ");
   stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
   data.buffer.push("\n</div>\n\n<div id=\"fb-root\"></div>\n");
@@ -82,10 +82,10 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   var buffer = '', stack1, helper, options, escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing;
 
 
-  data.buffer.push("<li class=\"audio\" ");
+  data.buffer.push("<li ");
   data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
-    'data-listened': ("episode.listened")
-  },hashTypes:{'data-listened': "ID"},hashContexts:{'data-listened': depth0},contexts:[],types:[],data:data})));
+    'class': (":audio episode.listened episode.playing")
+  },hashTypes:{'class': "STRING"},hashContexts:{'class': depth0},contexts:[],types:[],data:data})));
   data.buffer.push(" ");
   data.buffer.push(escapeExpression(helpers['bind-attr'].call(depth0, {hash:{
     'data-id': ("episode.id")
