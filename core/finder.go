a98f4fdd7f0614e993de0e48727b295aba8a2c8a
Duke <duke@riseup.net>
2014-03-12 18:45:37 -0300

    added missing finder.go

---
+++ core/finder.go
@@ -0,0 +1,63 @@
+package core
+
+import (
+	//"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	// "code.google.com/p/go.net/html"
+	r "github.com/dukex/uhura/core/helper"
+)
+
+func FindChannels(userId string, w http.ResponseWriter, request *http.Request) {
+	channels := make([]ChannelEntity, 0)
+	url := request.PostFormValue("url")
+	pageR, _ := http.Get(url)
+	contentype := pageR.Header.Get("Content-Type")
+	isXml := (strings.Contains(contentype, "xml") && !strings.Contains(contentype, "html"))
+
+	if isXml {
+		body, _ := ioutil.ReadAll(pageR.Body)
+		FetchOnlyChannelFromBytes(url, body)
+		database.Scopes(ChannelDefaultQuery(userId)).Where("url = ?", url).Scan(&channels)
+	} else {
+		// channels = FetchTempChannelFromLinks(findLinks(pageR.Body))
+	}
+
+	r.ResponseJSON(w, 200, map[string]interface{}{"channels": channels})
+}
+
+// func findLinks(body io.Reader) []string {
+// 	h, _ := html.Parse(body)
+
+// 	links := make([]*html.Node, 0)
+// 	feeds := make([]string, 0)
+
+// 	var l func(n *html.Node)
+// 	l = func(n *html.Node) {
+// 		if n.Type == html.ElementNode && n.Data == "link" {
+// 			for _, link := range n.Attr {
+// 				if link.Key == "type" && link.Val == "application/rss+xml" {
+// 					links = append(links, n)
+// 				}
+// 			}
+// 		}
+
+// 		for c := n.FirstChild; c != nil; c = c.NextSibling {
+// 			l(c)
+// 		}
+// 	}
+
+// 	l(h)
+
+// 	for _, link := range links {
+// 		for _, a := range link.Attr {
+// 			if a.Key == "href" {
+// 				feeds = append(feeds, a.Val)
+// 			}
+// 		}
+// 	}
+
+// 	return feeds
+// }
