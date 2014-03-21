de996015eb4809b462d7e4d0bfa75c00102540bf
Duke <duke@riseup.net>
2014-03-21 01:28:16 -0300

    removed handlers file

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -25,6 +26,8 @@ var (
 	URL            string
 	loginBuilder   *auth.Builder
 	MIXPANEL       *core.Mixpanel
+	EMAIL          string
+	PAGES          map[string][]byte
 )
 
 // Helpers
@@ -110,7 +113,63 @@ func getGitSHA() string {
 	return sha
 }
 
+// Handlers
+func LandingHandler(w http.ResponseWriter, r *http.Request) {
+	if loginBuilder.CurrentUser(r) != "" {
+		http.Redirect(w, r, "/app/", 302)
+	} else {
+		fmt.Fprintf(w, BuildPage("index"))
+	}
+}
+
+func EnterHandler(w http.ResponseWriter, request *http.Request) {
+	EMAIL = request.FormValue("email")
+	exists := core.UserExists(EMAIL)
+	if exists {
+		http.Redirect(w, request, "/login?user=exists", 302)
+	} else {
+		fmt.Fprintf(w, BuildPage("users/sign_up"))
+	}
+}
+
+func AppHandler(userId string, w http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(w, BuildPage("app"))
+}
+
+func LoginHandler(w http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(w, BuildPage("users/sign_in"))
+}
+
+// helpers
+
+func BuildPage(page string) string {
+	if ENV == "development" {
+		buildPageFromFile(page)
+	}
+
+	var pageBytes []byte
+	pageBytes, ok := PAGES[page]
+
+	if !ok {
+		pageBytes, _ = buildPageFromFile(page)
+	}
+
+	pageHTML := string(pageBytes[:])
+	pageHTML = strings.Replace(pageHTML, "<% EMAIL %>", EMAIL, -1)
+	pageHTML = strings.Replace(pageHTML, "<% URL %>", URL, -1)
+	pageHTML = strings.Replace(pageHTML, "<% ASSETS_VERSION %>", ASSETS_VERSION, -1)
+
+	return pageHTML
+}
+
+func buildPageFromFile(page string) ([]byte, error) {
+	pBytes, err := ioutil.ReadFile("./views/" + page + ".html")
+	PAGES[page] = pBytes
+	return pBytes, err
+}
+
 func main() {
+	PAGES = make(map[string][]byte, 0)
 	ASSETS_VERSION = getGitSHA()
 	ENV = os.Getenv("ENV")
 	PORT = os.Getenv("PORT")
