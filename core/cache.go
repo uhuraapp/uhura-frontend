389871c1ca304d78b2a0e35bf8cbf1b7259784e8
Duke <duke@riseup.net>
2014-04-12 12:44:21 -0300

    updated cache.go

--- core/cache.go
+++ core/cache.go
@@ -11,28 +11,31 @@ import (
 )
 
 var (
-	cache  *memcache.Client
-	writer io.Writer
-	reader io.Reader
+	CACHE *memcache.Client
 )
 
 func CacheSet(key string, data interface{}) {
 	var (
 		value []byte
 		mh    codec.MsgpackHandle
+		w     io.Writer
 	)
 
 	mh.MapType = reflect.TypeOf(data)
+	mh.SliceType = reflect.TypeOf(data)
 
-	enc := codec.NewEncoder(writer, &mh)
+	enc := codec.NewEncoder(w, &mh)
 	enc = codec.NewEncoderBytes(&value, &mh)
 	err := enc.Encode(data)
 
 	if err == nil {
-		_, err = cache.Set(0, key, 0, 0, value)
-		log.Println("CACHE ERROR:", err)
+		_, err = CACHE.Set(0, key, 0, 0, value)
+		log.Println("Caching", key, "--", mh.MapType, " -->", data)
+		if err != nil {
+			log.Println("CACHE ERROR:", err)
+		}
 	} else {
-		log.Println("CACHE ERROR:", err)
+		log.Println("ENCODE ERROR:", err)
 	}
 }
 
@@ -40,18 +43,22 @@ func CacheGet(key string, as interface{}) (interface{}, error) {
 	var (
 		data interface{}
 		mh   codec.MsgpackHandle
+		r    io.Reader
 	)
 
 	mh.MapType = reflect.TypeOf(as)
+	mh.SliceType = reflect.TypeOf(as)
 
-	cached, err := cache.Get(0, key)
+	cached, err := CACHE.Get(0, key)
 	if err != nil {
 		return nil, err
 	}
 
-	dec := codec.NewDecoder(reader, &mh)
+	dec := codec.NewDecoder(r, &mh)
 	dec = codec.NewDecoderBytes(cached.Body, &mh)
 	err = dec.Decode(&data)
+
+	log.Println("Getting", key, "--", mh.MapType, " -->", data)
 
 	return data, err
 }
@@ -61,16 +68,13 @@ func init() {
 	memcachedPassword := os.Getenv("MEMCACHEDCLOUD_PASSWORD")
 	memcachedUsername := os.Getenv("MEMCACHEDCLOUD_USERNAME")
 
-	cache, err := memcache.Connect("tcp", memcachedUrl)
-	if err != nil {
-		panic(err)
-	}
+	CACHE, _ = memcache.Connect("tcp", memcachedUrl)
 
 	if memcachedPassword != "" {
-		cache.Auth(memcachedUsername, memcachedPassword)
+		CACHE.Auth(memcachedUsername, memcachedPassword)
 	}
 
-	log.Println("CACHE Healthy", cache.IsHealthy())
-	log.Println(cache.Set(0, "test", 0, 5, []byte("1")))
-	log.Println(cache.Get(0, "test"))
+	log.Println("CACHE Healthy", CACHE.IsHealthy())
+	log.Println(CACHE.Set(0, "test", 0, 5, []byte("Testing...")))
+	log.Println(CACHE.Get(0, "test"))
 }
