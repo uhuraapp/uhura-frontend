e60dcd1cbda04858a4a2ba2e7150185820c941a9
Duke <duke@riseup.net>
2014-04-23 17:11:41 -0300

    fixed listened

--- assets/javascripts/app/views/episode_list_item.js
+++ assets/javascripts/app/views/episode_list_item.js
@@ -2,7 +2,7 @@ App.EpisodeListItemComponent = Ember.Component.extend({
   layoutName: "component/episode-list-item",
   click: function(e){
     element  = $(e.toElement)
-    if(element.is(".listened")) {
+    if(element.is("button.listened")) {
       App.PLAYER.listened(this.episode);
     }
   }
