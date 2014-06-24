b69f30941c85db48f0b23383705193bd9b8fcd0a
Duke <duke@riseup.net>
2014-06-24 04:56:55 -0300

    added SubscriptionsByUrl tasker

--- core/queues.go
+++ core/queues.go
@@ -7,6 +7,29 @@ import (
 	"github.com/streadway/amqp"
 )
 
+type SubscriptionsByUrl struct {
+	Tasker
+}
+
+func (s *SubscriptionsByUrl) Perform(d amqp.Delivery) {
+	var channel Channel
+
+	data := strings.Split(string(d.Body), "|")
+	notFound := database.Table("channels").Where("url = ?", data[1]).First(&channel).RecordNotFound()
+
+	if notFound {
+		FetchChannel(data[1])
+		d.Nack(true, true)
+	} else {
+		SubscribeChannelHelper(data[0], strconv.Itoa(int(channel.Id)))
+		d.Ack(false)
+	}
+}
+
+func (s *SubscriptionsByUrl) QueueName() string {
+	return "subscriptions_by_url"
+}
+
 // -- define tasks
 
 func init() {
