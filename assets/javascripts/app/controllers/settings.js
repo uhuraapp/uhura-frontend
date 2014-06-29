7ff25fb85cda8445d9ab844945abcb0fb09f4ecc
Duke <duke@riseup.net>
2014-06-28 23:33:27 -0300

    added import on settins page

--- assets/javascripts/app/controllers/settings.js
+++ assets/javascripts/app/controllers/settings.js
@@ -1,12 +1,12 @@
 /* global App, $, Ember */
 App.SettingsController = Ember.ObjectController.extend({
   importChannels: [],
+  imported: false,
   actions: {
     addImportItem: function (channels) {
       for (var i = channels.length - 1; i >= 0; i--) {
         channels[i].temp_id = i;
       }
-
       this.set('importChannels', channels);
     },
     removeImportItem: function (temp_id) {
@@ -20,17 +20,18 @@ App.SettingsController = Ember.ObjectController.extend({
       this.set('importChannels', channels);
       $("[data-temp-id='"+temp_id+"']").remove();
     },
-    importChannelsAndSubscribe: function() {
-      $.post({
-        url: '/api/batch/subscriptions',
-        data: {channels: this.get('importChannels')},
-        success: function() {
-          this.set('importChannels', []);
-        },
-        error: function() {
-          alert("Error");
-        }
-      });
+    importChannelsAndSubscribe: function () {
+      var _this = this,
+          urls  = _.map(this.get('importChannels'), function(c) { return c.url; });
+
+      $.post("/api/batch/subscriptions-by-url", {urls: urls})
+        .done(function () {
+          _this.set('importChannels', []);
+          _this.set('imported', true);
+        })
+        .fail(function () {
+          alert("error");
+        });
     }
   }
 });
