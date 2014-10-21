50d6c1e4f3b3ca135d4f8d3fc97c79acfdd70136
Duke <duke@riseup.net>
2014-10-21 21:27:01 -0200

    added try/catch to subscribed

--- assets/javascripts/app/views/channel_cover_component.js
+++ assets/javascripts/app/views/channel_cover_component.js
@@ -4,11 +4,20 @@ App.ChannelCoverComponent = Ember.Component.extend({
   tagName: "li",
   actions: {
     subscribeChannel: function(_id) {
-      this.get('channel').set('subscribed', true);
+      try {
+        var c = this.get('channel');
+        if(c) c.set('subscribed', true);
+      } catch (e) {
+      }
       Subscriptions.subscribe.call(this, _id);
     },
     unsubscribeChannel:  function(_id){
-      this.get('channel').set('subscribed', false);
+      try {
+        var c = this.get('channel');
+        if(c) c.set('subscribed', false);
+      } catch (e) {
+      }
+
       Subscriptions.unsubscribe.call(this, _id);
     }
   }
