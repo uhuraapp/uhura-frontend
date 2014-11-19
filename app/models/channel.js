092d65fc2821e3203b7c4e593c7abf29dcdc3c8a
Duke <dukekhaos@gmail.com>
2014-11-19 09:09:20 -0200

    started channel profile

--- app/models/channel.js
+++ app/models/channel.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string')
+  title: DS.attr('string'),
+  image_url: DS.attr('string'),
+  description: DS.attr('string'),
+  copyrigth: DS.attr('string')
 });
