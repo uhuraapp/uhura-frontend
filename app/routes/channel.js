def3b604a34d25a92aeff097e7a3d21c0ed45eba
Duke <dukekhaos@gmail.com>
2014-11-20 21:31:53 -0200

    updated activate to channel route

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  activate: function () {
+    var title = this.modelFor('channel').get('title')
+
+    $(document).attr('title', title);
+    $("[property='og:title']").attr('content', title);
+
+    $.UIkit.offcanvas.hide(true)
+  }
 });
