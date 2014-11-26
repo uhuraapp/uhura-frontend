269231732166bf7d0a1a7ab09fde22170fafef2f
Duke <dukekhaos@gmail.com>
2014-11-26 09:51:10 -0200

    added description to episode item

--- app/components/episode-list-item.js
+++ app/components/episode-list-item.js
@@ -1,6 +1,14 @@
+/* global $ */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: "li",
-  classNames: ["episode"]
+  classNames: ["episode"],
+  actions: {
+    info: function() {
+      var t = $(event.target);
+      t.toggleClass('active');
+      t.parents('li').find('.description').toggleClass('uk-hidden');
+    }
+  }
 });
--- app/models/episode.js
+++ app/models/episode.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 
 
 export default DS.Model.extend({
-  title: DS.attr('string')
+  title: DS.attr('string'),
+  description: DS.attr('string')
 });
