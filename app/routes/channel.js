bb0e9251be85c8f5198a235dcf93cc98592f28d2
Duke <dukekhaos@gmail.com>
2014-11-26 09:27:31 -0200

    improved channel profile, episode card better to smartphone

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -1,12 +1,13 @@
+/* global $ */
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   activate: function () {
-    var title = this.modelFor('channel').get('title')
+    var title = this.modelFor('channel').get('title');
 
     $(document).attr('title', title);
     $("[property='og:title']").attr('content', title);
 
-    $.UIkit.offcanvas.hide(true)
+    $.UIkit.offcanvas.hide(true);
   }
 });
