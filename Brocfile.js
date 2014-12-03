f7bc94d508e2b9b9e4602ea9547e0f68e7193d0c
Duke <dukekhaos@gmail.com>
2014-12-03 09:22:51 -0200

    added mediaelement

--- Brocfile.js
+++ Brocfile.js
@@ -13,6 +13,14 @@ app.import("bower_components/jquery.cookie/jquery.cookie.js");
 app.import("bower_components/uikit/scss/uikit.scss");
 app.import("bower_components/uikit/js/uikit.js");
 
+app.import("bower_components/mediaelement/build/mediaelement-and-player.js");
+app.import("bower_components/mediaelement/build/mediaelementplayer.css");
+app.import("bower_components/mediaelement/build/controls.svg", {destDir: 'assets'});
+app.import("bower_components/mediaelement/build/controls.fw.png", {destDir: 'assets'});
+app.import("bower_components/mediaelement/build/controls.png", {destDir: 'assets'});
+app.import("bower_components/mediaelement/build/controls-wmp.png", {destDir: 'assets'});
+app.import("bower_components/mediaelement/build/controls-wmp-bg.png", {destDir: 'assets'});
+
 app.import("bower_components/typicons/src/font/typicons.css", {destDir: 'assets'});
 app.import("bower_components/typicons/src/font/typicons.ttf", {destDir: 'assets'});
 app.import("bower_components/typicons/src/font/typicons.svg", {destDir: 'assets'});
@@ -26,10 +34,7 @@ app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", {destD
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", {destDir: 'fonts'});
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", {destDir: 'fonts'});
 
-
-
 var tree = app.toTree();
-
 
 compileSass([tree], 'assets/app.sass', 'assets/app.css');
 
