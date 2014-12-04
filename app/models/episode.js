f0377ddb58cedee425def6466a91d7a961dbc53b
Duke <dukekhaos@gmail.com>
2014-12-04 09:41:08 -0200

    updated episode model, episode belongs to channel

--- app/models/episode.js
+++ app/models/episode.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   description: DS.attr('string'),
-  source_url: DS.attr('string')
+  source_url: DS.attr('string'),
+  channel: DS.belongsTo('channel')
 });
