3d10f5f53d01d8176bea0f96fa4ca36e129f3c83
Duke <dukekhaos@gmail.com>
2014-12-06 19:55:48 -0200

    added loading inital

---
+++ app/views/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  didInsertElement: function() {
+    this._super();
+    Ember.run.scheduleOnce('afterRender', this, function(){
+      $("#loading").fadeOut();
+    });
+  }
+});
