a809df73c6ad27484f17948d69fa30cf53efdc97
Duke <dukekhaos@gmail.com>
2014-12-07 11:18:53 -0200

    added listened behavior and fixed media element options

--- app/controllers/player.js
+++ app/controllers/player.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Config from '../config/environment';
 
 export default Ember.ObjectController.extend({
   playerChanges: function() {
@@ -13,11 +14,26 @@ export default Ember.ObjectController.extend({
     }
   }.observesBefore('model'),
 
+  listenedChanges: function() {
+    var model = this.get('model');
+    if (this.get('playing')) {
+      this.send('listened', model.id)
+    }
+  }.observes('model.listened'),
+
   actions: {
     playpause: function () {
       var audio = this.get('audio');
       this.get('playing') ? audio.pause() : audio.play();
       this.set('playing', !this.get('playing'));
+    },
+    listened: function(modelId) {
+      $.get(
+        Config.API_URL +
+        '/api/v2/episodes/' +
+        modelId +
+        '/listened'
+      )
     }
   }
 });
--- app/views/player.js
+++ app/views/player.js
@@ -10,14 +10,43 @@ export default Ember.View.extend({
     if(model){
       var audio = this.$('audio');
       audio.attr('src', model.get('source_url'));
-      audio.mediaelementplayer(this.get('mediaOptions'));
+      audio.mediaelementplayer({
+        alwaysShowControls: true,
+        audioVolume: 'horizontal',
+        features: ['playpause','progress','volume', 'current'],
+        success: this.successCallback(this)
+      });
       controller.set('audio', audio[0]);
+      controller.set('loading', false);
       this.set('hasModel', true);
     }
   }.observes('controller.model'),
+  successCallback: function(_this){
+    return function(media, node, player) {
+      var controller = _this.get('controller');
+
+      media.addEventListener('timeupdate', function (e) {
+        var playing = Math.round(100 * media.currentTime / media.duration);
+        if (playing > 95) {
+          controller.get('model').set('listened', true)
+        }
+      }, false);
+
+      /*
+      media.addEventListener('pause', function(e) {
+        App.PLAYER.isPlaying  = false;
+        App.PLAYER.current.set("playing", false);
+      }, false);
+
+      media.addEventListener('play', function(e) {
+        App.PLAYER.isPlaying  = true;
+        App.PLAYER.current.set("playing", true);
+      }, false);
+
+      media.addEventListener('ended', App.PLAYER.APIS.audio.nextTrack);
+      */
+    }
+  },
   mediaOptions: {
-    alwaysShowControls: true,
-    audioVolume: 'horizontal',
-    features: ['playpause','progress','volume']
   }
 });
