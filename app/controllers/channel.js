2b52ef7cd521cef81be59c32f3ef20fd85b4cfcb
Duke <dukekhaos@gmail.com>
2014-12-09 00:05:49 -0200

    fixed markAllAsListened

--- app/controllers/channel.js
+++ app/controllers/channel.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   actions: {
-     markAllAsListened: function () {
-        var i, episodes = this.get('model').get('episodes');
-        for(i = 0; i < episodes.length; i++) {
-            episodes[i].set ('listened', true)
-        }
-     }
+    markAllAsListened: function () {
+      debugger;
+      var i, episodes = this.get('model.episodes').content;
+      for(i = 0; i < episodes.length; i++) {
+        episodes[i].set ('listened', true);
+      }
+    }
   }
-})
+});
