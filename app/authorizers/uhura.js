4485a1aab69a336c6dae232a73d9c3a093355878
Duke <dukekhaos@gmail.com>
2014-12-20 21:46:17 -0200

    fixed Token header

--- app/authorizers/uhura.js
+++ app/authorizers/uhura.js
@@ -3,6 +3,6 @@ import Base from 'simple-auth/authorizers/base';
 export default Base.extend({
   authorize: function(jqXHR) {
     var token = this.get('session.token');
-    jqXHR.setRequestHeader('Authorization', "Token token=\""+ token +"\"");
+    jqXHR.setRequestHeader('Authorization', "Token "+ token);
   }
 });
