7a8697f8d0962090b26badfd3024694bcbe1df7b
Duke <dukekhaos@gmail.com>
2014-12-23 03:36:46 -0200

    after auth reload application route

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,10 @@ export default Ember.Route.extend({
   beforeModel: function() {
     if(!this.get('session.isAuthenticated')) {
       this.transitionTo("login");
+    } else {
+      var routeA = this.container.lookup("route:application");
+      var controllerA = this.container.lookup("controller:application");
+      routeA.model().then(function(data){ controllerA.set("model", data)});
     }
   }
 });
