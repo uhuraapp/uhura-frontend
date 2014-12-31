7197e8ee2c199f0ba811c4f5250464786363f51e
Duke <dukekhaos@gmail.com>
2014-12-31 11:08:47 -0200

    fix(adapter): fix adapter path for user urls

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,16 @@ import ENV from '../config/environment';
 
 export default DS.RESTAdapter.extend({
   namespace: 'v2',
-  host: ENV.API_URL
+  host: ENV.API_URL,
+  pathForType: function(type){
+    var path = this._super(type);
+
+    if(this.__isUserPath(type)){
+      path = "users/"+path;
+    }
+    return path;
+  },
+  __isUserPath: function(type) {
+    return ['subscription', 'suggestion'].indexOf(type) > -1;
+  }
 });
