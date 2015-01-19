1a209c1031a485f7f08a9117b9fc40da9d3a6fe3
Duke <dukekhaos@gmail.com>
2015-01-19 02:51:36 -0200

    fixed categories

--- app/models/category.js
+++ app/models/category.js
@@ -1,9 +1,27 @@
 import DS from 'ember-data';
 
+
+var imageResizedURL = function(imageURL) {
+  var size, imageWidth, windowWidth = window.screen.width, resizeServer = "http://uhura-img.herokuapp.com";
+
+  if(windowWidth < 401) {
+    imageWidth = "400";
+  } else if(windowWidth < 701) {
+    imageWidth = "900";
+  } else if(windowWidth < 901) {
+    imageWidth = "900";
+  } else  if(windowWidth > 900) {
+    return imageURL;
+  }
+
+  size = imageWidth + "/" + imageWidth;
+  return resizeServer + "/" + encodeURIComponent(imageURL) + "/" + size;
+};
+
 export default DS.Model.extend({
   name: DS.attr(),
   channel: DS.attr(),
   thumb_url: function(){
-    return this.get('channel').image_url;
+    return imageResizedURL(this.get('channel.image_url'));
   }.property('channel')
 });
