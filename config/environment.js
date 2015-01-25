e316b8304d79137faea98217a7d531bc31ae6203
Duke <dukekhaos@gmail.com>
2015-01-24 22:52:21 -0200

    add android files

--- config/environment.js
+++ config/environment.js
@@ -15,6 +15,11 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    cordova: {
+      rebuildOnChange: false,
+      emulate: false
     }
   };
 
