e4f188f120b6487c9c30e1fdaaa654c687839bc4
Duke <dukekhaos@gmail.com>
2015-03-02 10:35:12 -0300

    fix: remove fixSidebarHeight

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,8 @@
-/* global fixSidebarHeight */
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  activate: function() {
-    setTimeout(fixSidebarHeight, 20);
-  },
   beforeModel: function() {
-    if(!this.get('session.isAuthenticated')) {
-      this.transitionTo("login");
-    } else {
+    if(this.get('session.isAuthenticated')) {
       var routeA = this.container.lookup("route:application");
       var controllerA = this.container.lookup("controller:application");
       routeA.model().then(function(data){ controllerA.set("model", data); });
