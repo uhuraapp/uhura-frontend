cfb30cee4733ae0e953c07220e5335440dcdf4c9
Duke <dukekhaos@gmail.com>
2015-03-04 03:07:41 -0300

    fixed transitions on routes index

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,7 +5,16 @@ export default Ember.Route.extend({
     if(this.get('session.isAuthenticated')) {
       var routeA = this.container.lookup("route:application");
       var controllerA = this.container.lookup("controller:application");
-      routeA.model().then(function(data){ controllerA.set("model", data); });
+      var __this = this;
+      routeA.model().then(function(data){
+        if(data.get('length') > 0){
+          controllerA.set("model", data);
+        } else {
+          __this.transitionTo('channels');
+        }
+      });
+    } else {
+      this.transitionTo('login');
     }
   }
 });
