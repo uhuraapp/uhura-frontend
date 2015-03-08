98f5eda28b22e0e8d0ef04cd4aa7697c3bc0d385
Duke <dukekhaos@gmail.com>
2015-03-08 15:07:02 -0300

    only send Authorization header when has token

--- app/authorizers/uhura.js
+++ app/authorizers/uhura.js
@@ -3,6 +3,8 @@ import Base from 'simple-auth/authorizers/base';
 export default Base.extend({
   authorize: function(jqXHR) {
     var token = this.get('session.token');
-    jqXHR.setRequestHeader('Authorization', "Token "+ token);
+    if(token !== undefined || token !== null || token !== null) {
+      jqXHR.setRequestHeader('Authorization', "Token "+ token);
+    }
   }
 });
