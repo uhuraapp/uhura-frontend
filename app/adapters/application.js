ca5ccca230dfe74d1ec3c842c8d8c1f27a773a6c
Duke <dukekhaos@gmail.com>
2015-03-14 14:24:14 -0300

    refactor adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,12 +5,7 @@ export default DS.RESTAdapter.extend({
   namespace: 'v2',
   host: ENV.API_URL,
   pathForType: function(type){
-    var path = this._super(type);
-
-    if(this.__isUserPath(type)){
-      path = "users/"+path;
-    }
-    return path;
+    return (this.__isUserPath(type) ? "users/" : "") + this._super(type);
   },
   __isUserPath: function(type) {
     return ['subscription', 'suggestion'].indexOf(type) > -1;
