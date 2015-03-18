5030ab0ab41312e22b0a7e3c218ecd02c99d972d
Duke <dukekhaos@gmail.com>
2015-03-18 01:49:11 -0300

    cache channels

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+import cacheChannel from '../utils/cache-channel';
 
 export default Ember.ArrayController.extend({
+  cacheChannel: function () {
+    var model = this.get('model');
+    cacheChannel(this.emberSync, model[model.length - 1]);
+  }.observes('@each')
 });
