393b0aa178e05886e860b143f85a22c9b2579082
Duke <dukekhaos@gmail.com>
2015-03-22 05:08:51 -0300

    remove cordova ifs

--- app/controllers/downloader.js
+++ app/controllers/downloader.js
@@ -11,7 +11,11 @@ export default Ember.Controller.extend({
     };
   },
   storePath: function() {
-    return cordova.file.externalDataDirectory || cordova.file.dataDirectory;
+    if(window.cordova){
+      return cordova.file.externalDataDirectory || cordova.file.dataDirectory;
+    } else {
+      return "/"
+    }
   },
   download: function(episode){
     return () => {
@@ -37,26 +41,15 @@ export default Ember.Controller.extend({
 
   __checkFile: function (episode) {
     return new Ember.RSVP.Promise( (resolve, reject) => {
-      if(window.cordova) {
-        window.resolveLocalFileSystemURL(this.storePath() + episode.id, resolve, reject);
-      } else {
-        console.log("TODO __checkFile");
-        reject();
-      }
+      window.resolveLocalFileSystemURL(this.storePath() + episode.id, resolve, reject);
     });
   },
 
   __downloadFile: function (episode) {
     return new Ember.RSVP.Promise( (resolve, reject) => {
       var assetURL = config.API_URL + '/v2/episodes/' + episode.id + '/download';
-
-      if(window.cordova) {
-        var fileTransfer = new FileTransfer();
-        fileTransfer.download(assetURL, this.storePath() + episode.id, resolve, reject);
-      } else {
-        console.log("TODO __downloadFile");
-        reject();
-      }
+      var fileTransfer = new FileTransfer();
+      fileTransfer.download(assetURL, this.storePath() + episode.id, resolve, reject);
     });
   }
 });
