45c9053f321da1378894268e27d1224a625001dd
Duke <dukekhaos@gmail.com>
2015-03-31 08:11:36 -0300

    fix: remove mark all as listened button when you dont have episodes to listen

--- app/controllers/channel.js
+++ app/controllers/channel.js
@@ -4,16 +4,23 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
  removeListened: true,
 
- lastEpisodes: [],
+ filterEpisodes: function(filter) {
+   var episodes = this.get('model.episodes').sortBy('published_at').reverse();
+   return episodes.filter(filter);
+ },
 
  episodesFiltered: function(){
-    var removeListened = this.get('removeListened');
-    var episodes = this.get('model.episodes').sortBy('published_at').reverse();
+   var removeListened = this.get('removeListened');
+   return this.filterEpisodes( (episode) => {
+     return !(removeListened && episode.get('listened'));
+   })
+ }.property('model.episodes.@each.listened', 'removeListened'),
 
-    return episodes.filter( (episode) => {
-      return !(removeListened && episode.get('listened'));
-    });
-  }.property('model.episodes.@each.listened', 'removeListened'),
+ showMarkAllAsListened: function () {
+   return this.filterEpisodes( (episode) => {
+     return !episode.get('listened');
+   }).length > 0
+ }.property('model.episodes.@each.listened'),
 
   actions: {
     markAllAsListened: function () {
