bbbc5e165aab19a476b6c21c4c9a791261dc4121
Duke <dukekhaos@gmail.com>
2015-04-17 02:52:15 -0300

    fix slideout call

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,18 +4,20 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   actions : {
     createMenu: function () {
-      this.slideout = new Slideout({
-        'panel': document.getElementById('content'),
-        'menu': document.getElementById('menu'),
-        'padding': 256,
-        'tolerance': 70
-      });
-    },
-    toggleMenu: function () {
-      this.slideout.toggle();
-    },
-    closeMenu: function () {
-      if(this.slideout) {  this.slideout.close(); }
+      if(this.get('session.isAuthenticated')) {
+        this.slideout = new Slideout({
+          'panel': document.getElementById('content'),
+          'menu': document.getElementById('menu'),
+          'padding': 256,
+          'tolerance': 70
+        });
+      }
     }
+  },
+  toggleMenu: function () {
+    if(this.slideout) {  this.slideout.toggle(); }
+  },
+  closeMenu: function () {
+    if(this.slideout) {  this.slideout.close(); }
   }
 });
