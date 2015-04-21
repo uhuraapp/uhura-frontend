0149e88455aefee19454f4333e8df9134ad97023
Duke <dukekhaos@gmail.com>
2015-04-21 20:27:26 -0300

    lazy load explore images

--- app/views/explore.js
+++ app/views/explore.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.View.extend({
   grid: function () {
     UIkit.grid($('#categories'), {gutter: 30});
-  }.on("didInsertElement")
+    Ember.run.scheduleOnce("afterRender", this.__loadLazyImages);
+  }.on("didInsertElement"),
+
+  resultsChanges: function () {
+    Ember.run.scheduleOnce("afterRender", this.__loadLazyImages);
+  }.observes('controller.results'),
+
+  __loadLazyImages: function () {
+    var layzr = new Layzr({
+      selector: '[data-layzr]'
+    });
+  }
 });
