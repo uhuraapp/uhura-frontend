8a48f8ad715d9c51e90995b7299e4623671dc1c9
Duke <dukekhaos@gmail.com>
2015-04-22 01:01:30 -0300

    add fallback image to channels

--- app/views/explore.js
+++ app/views/explore.js
@@ -11,8 +11,18 @@ export default Ember.View.extend({
   }.observes('controller.results'),
 
   __loadLazyImages: function () {
+    var check = function (img) {
+      if (typeof img.naturalWidth !== "undefined" && img.naturalWidth === 0) {
+        img.setAttribute("src", img.getAttribute('data-fallback'))
+      }
+    };
+
     var layzr = new Layzr({
-      selector: '[data-layzr]'
+      selector: '[data-layzr]',
+      callback: function (node) {
+        if(!this.complete) {  this.onload = this.onerror = function () { check(this); };
+        } else { check(this); }
+      }
     });
   }
 });
