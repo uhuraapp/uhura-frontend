df5b7377f7762ecc371c1ce628c29cbdafba2ae5
Duke <dukekhaos@gmail.com>
2015-04-23 02:21:42 -0300

    remove early cache for now

--- app/initializers/cache-data.js
+++ app/initializers/cache-data.js
@@ -3,17 +3,16 @@ import Ember from 'ember';
 export function initialize( container, application ) {
   application.deferReadiness();
 
-  var store = container.lookup('store:main');
-
-  Ember.RSVP.Promise.resolve().then(function(){
-    return store.find('episode');
-  }).then(function() {
-    return store.find('channel');
-  }).then(function(){
-    return store.find('subscription');
-  }).then(function(){
+  // var store = container.lookup('store:main');
+  // Ember.RSVP.Promise.resolve().then(function(){
+  //  return store.find('episode');
+  // }).then(function() {
+  //  return store.find('channel');
+  // }).then(function(){
+  //  return store.find('subscription');
+  // }).then(function(){
     application.advanceReadiness();
-  });
+  // });
 }
 
 export default {
