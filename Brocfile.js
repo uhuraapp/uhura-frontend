76c51734743f2415d9534571afd54eee6541ee78
Duke <dukekhaos@gmail.com>
2015-04-24 02:00:24 -0300

    add player style again

--- Brocfile.js
+++ Brocfile.js
@@ -11,21 +11,44 @@ var app = new EmberApp({
   },
 });
 
+app.import("bower_components/uikit/js/uikit.js");
+app.import("bower_components/uikit/js/components/grid.js");
+app.import("bower_components/lunr.js/lunr.js");
+app.import("bower_components/layzr.js/dist/layzr.js");
+app.import("bower_components/ember-indexeddb-adapter/dist/ember_indexeddb_adapter.js");
+app.import("bower_components/moment/min/moment-with-locales.js");
+app.import("bower_components/fastclick/lib/fastclick.js")
+app.import("bower_components/mediaelement/build/mediaelement-and-player.js");
+
+app.import("bower_components/mediaelement/build/mediaelementplayer.css");
+
+app.import("bower_components/uhura-i18n/dist/i18n.amd.js", {
+  exports: { "i18n": [ "default", "lang/pt" ] }
+});
+
+//
+// app.import("bower_components/slideout/dist/slideout.js");
+//
+
+app.import("bower_components/typicons/src/font/typicons.css", {destDir: 'assets'});
+app.import("bower_components/typicons/src/font/typicons.ttf", {destDir: 'assets'});
+app.import("bower_components/typicons/src/font/typicons.svg", {destDir: 'assets'});
+app.import("bower_components/typicons/src/font/typicons.eot", {destDir: 'assets'});
+app.import("bower_components/typicons/src/font/typicons.woff", {destDir: 'assets'});
+app.import("bower_components/font-awesome/css/font-awesome.css");
+app.import("bower_components/font-awesome/fonts/FontAwesome.otf", {destDir: 'fonts'});
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", {destDir: 'fonts'});
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", {destDir: 'fonts'});
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", {destDir: 'fonts'});
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", {destDir: 'fonts'});
+app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", {destDir: 'fonts'});
 
 // app.import("bower_components/es6-shim/es6-sham.js")
 // app.import("bower_components/es6-shim/es6-shim.js")
 //
 // app.import("bower_components/filer.js/src/filer.js");
 //
-app.import("bower_components/uikit/js/uikit.js");
-app.import("bower_components/uikit/js/components/grid.js");
 
-app.import("bower_components/lunr.js/lunr.js");
-
-app.import("bower_components/layzr.js/dist/layzr.js");
-//
-// app.import("bower_components/mediaelement/build/mediaelement-and-player.js");
-// app.import("bower_components/mediaelement/build/mediaelementplayer.css");
 //
 // app.import("bower_components/mediaelement/build/flashmediaelement.swf", {destDir: 'assets'});
 // app.import("bower_components/mediaelement/build/silverlightmediaelement.xap", {destDir: 'assets'});
@@ -36,32 +59,6 @@ app.import("bower_components/layzr.js/dist/layzr.js");
 // app.import("bower_components/mediaelement/build/controls-wmp.png", {destDir: 'assets'});
 // app.import("bower_components/mediaelement/build/controls-wmp-bg.png", {destDir: 'assets'});
 //
-
-app.import("bower_components/typicons/src/font/typicons.css", {destDir: 'assets'});
-app.import("bower_components/typicons/src/font/typicons.ttf", {destDir: 'assets'});
-app.import("bower_components/typicons/src/font/typicons.svg", {destDir: 'assets'});
-app.import("bower_components/typicons/src/font/typicons.eot", {destDir: 'assets'});
-app.import("bower_components/typicons/src/font/typicons.woff", {destDir: 'assets'});
-
-app.import("bower_components/font-awesome/css/font-awesome.css");
-app.import("bower_components/font-awesome/fonts/FontAwesome.otf", {destDir: 'fonts'});
-app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", {destDir: 'fonts'});
-app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", {destDir: 'fonts'});
-app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", {destDir: 'fonts'});
-app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", {destDir: 'fonts'});
-app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", {destDir: 'fonts'});
-
-app.import("bower_components/uhura-i18n/dist/i18n.amd.js", {
-  exports: { "i18n": [ "default", "lang/pt" ] }
-});
-
-app.import("bower_components/ember-indexeddb-adapter/dist/ember_indexeddb_adapter.js");
-
-app.import("bower_components/moment/min/moment-with-locales.js");
-//
-// app.import("bower_components/slideout/dist/slideout.js");
-//
-app.import("bower_components/fastclick/lib/fastclick.js")
 
 var tree = app.toTree();
 
