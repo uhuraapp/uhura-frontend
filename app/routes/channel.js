03226889a8213fdef95c66397addc92e9d49881b
Duke <dukekhaos@gmail.com>
2015-04-25 23:25:34 -0300

    always fetch channel from server

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function (params) {
+    this.store.fetchById('channel', params.channel_id);
     return this.store.find('channel', params.channel_id);
   },
   setupController: function (controller, model) {
