1a60777c8050992926a73f78c7f11e4543824649
Duke <dukekhaos@gmail.com>
2015-04-28 07:50:30 -0300

    force reload on search by url

--- app/routes/channel-by-url.js
+++ app/routes/channel-by-url.js
@@ -5,6 +5,11 @@ export default Ember.Route.extend({
     var resourceURL = this.__adapter().buildURL('parser');
     return Ember.$.get(resourceURL + "/" + data.url);
   },
+  afterModel: function (model) {
+    if(model.url) {
+      window.location.reload();
+    }
+  },
   __adapter: function() {
     return this.store.adapterFor('channel');
   }
