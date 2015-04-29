8437bdb2c2c1fa3e8d24ef1406b409f7f11d9ac1
Duke <dukekhaos@gmail.com>
2015-04-29 00:30:33 -0300

    refactor episodes-list components

--- app/components/episodes-list.js
+++ app/components/episodes-list.js
@@ -8,23 +8,23 @@ export default Ember.Component.extend({
   page: 1,
   actions: {
     fetchMore: function(callback) {
-      var episodes = this.get('episodes'),
-        promise = Ember.RSVP.Promise.resolve(),
-        lastEpisode = (episodes.currentState ? episodes.currentState : episodes)[ episodes.length - 1 ],
-        since;
+      var promise = Ember.RSVP.Promise.resolve();
 
-      if(lastEpisode) {
-        since = moment.utc(lastEpisode.get('published_at')).format();
-      } else {
-        since = moment.utc(1999);
+      if(this.get('infiniteScroll')) {
+        var episodes = this.get('episodes'),
+            lastEpisode = (episodes.currentState ? episodes.currentState : episodes)[ episodes.length - 1 ];
+
+        var date = lastEpisode ? lastEpisode.get('published_at') : 1999;
+        var since = moment.utc(date).format();
+
+        // TODO: use meta
+        promise = this.store.find("episode", {
+          since: since,
+          channel_id: this.get('channel.raw_id'),
+          per_page: 20
+        });
+
       }
-
-      // TODO: use meta
-      promise = this.store.find("episode", {
-        since: since,
-        channel_id: this.get('channel.raw_id'),
-        per_page: 20
-      });
 
       callback(promise);
     }
