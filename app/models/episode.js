04a3ca1db995469e1c4cbeea1dfc7c1e77199cbb
Duke <dukekhaos@gmail.com>
2015-05-01 16:48:13 -0300

    add published at to episodes item

--- app/models/episode.js
+++ app/models/episode.js
@@ -10,9 +10,13 @@ export default DS.Model.extend({
   channel: DS.belongsTo('channel'),
   listened: DS.attr(),
   published_at: DS.attr('date'),
+  stopped_at: DS.attr('number'),
   source: function () {
     return this.get('source_url');
   }.property('source_url'),
+  publishedAt: function () {
+    return moment(this.get('published_at')).format("MMM Do YY");
+  }.property('published_at'),
   _makeListened: function () {
     var url = config.API_URL + '/v2/episodes/' + this.id + '/listened',
         method = !this.get('listened') ? "POST" : "DELETE";
@@ -21,7 +25,6 @@ export default DS.Model.extend({
       this.set('listened', !this.get('listened'));
     });
   }.observes('makeListened'),
-  stopped_at: DS.attr('number'),
   init: function () {
     this._super();
     this.set('progress', 0);
