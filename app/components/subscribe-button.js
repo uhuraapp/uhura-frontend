0db0f3a190c39fd49fa1adf7cff5bb123ca7b4db
Duke <dukekhaos@gmail.com>
2015-05-04 08:19:21 -0300

    add subscribed for visitants

--- app/components/subscribe-button.js
+++ app/components/subscribe-button.js
@@ -1,17 +1,25 @@
 import Ember from 'ember';
+import Login from '../services/login';
 
 export default Ember.Component.extend({
   classNames: ['subscribe-button'],
+  makeSubscription: function () {
+    var model = this.get('channel');
+    this.get('targetObject.store').createRecord('subscription', {
+      channel_id: model.id,
+      channel_url: model.links && model.links[0]
+    }).save().then(function(){
+      model.set('subscribed', true);
+    });
+  },
   actions: {
     subscribe: function() {
-      var model = this.get('channel');
-
-      this.get('targetObject.store').createRecord('subscription', {
-        channel_id: model.id,
-        channel_url: model.links && model.links[0]
-      }).save().then(function(){
-        model.set('subscribed', true);
-      });
+      if(this.get('session.isAuthenticated')) {
+        this.makeSubscription();
+      } else {
+        var login = new Login(this.container);
+        login.start(() => { this.makeSubscription(); });
+      }
     },
     unsubscribe: function() {
       var model = this.get('channel');
