85a6d7643391839c995af3f25a209a6cce8aad95
Duke <dukekhaos@gmail.com>
2015-06-04 15:20:32 -0300

    remove episodes filter

--- app/controllers/channel.js
+++ app/controllers/channel.js
@@ -1,38 +1,4 @@
-/* global $ */
 import Ember from 'ember';
-import uniqueBy from '../utils/unique-by';
 
 export default Ember.Controller.extend({
-  removeListened: true,
-
-  filterEpisodes: function(filter) {
-    var episodes = uniqueBy('id', this.get('model.episodes').toArray()).sortBy('published_at').reverse();
-    return episodes.filter(filter);
-  },
-
-  episodesFiltered: function(){
-    var removeListened = this.get('removeListened');
-    return this.filterEpisodes( (episode) => {
-      return !(removeListened && episode.get('listened'));
-    });
-  }.property('model.episodes.@each.listened', 'removeListened'),
-
-  allListenedButton: function () {
-    return this.get('session.isAuthenticated') && this.filterEpisodes( (episode) => {
-      return !episode.get('listened');
-    }).length > 0;
-  }.property('model.episodes.@each.listened'),
-
-  removeListenedButton: function () {
-    return this.get('session.isAuthenticated');
-  }.property('session.isAuthenticated'),
-
-  actions: {
-    markAllAsListened: function () {
-      var i, episodes = this.get('episodesFiltered');
-      for(i = 0; i < episodes.length; i++) {
-        $(".listened[data-id='" + episodes[i].id + "'][data-listened=false]").trigger('click');
-      }
-    }
-  }
 });
--- app/utils/unique-by.js
+++
@@ -1,6 +0,0 @@
-export default function uniqueBy(by, array) {
-  var o = {}, i, l = array.length, r = [];
-  for(i=0; i<l;i+=1){ o[array[i][by]] = array[i]; }
-  for(i in o){ r.push(o[i]); }
-  return r;
-}
