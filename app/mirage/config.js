a85bdee71acd4aa09fb68b3931ec11d8c78c7a96
Duke <dukekhaos@gmail.com>
2015-06-19 22:58:34 -0300

    add mirage hack to works with external api

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,4 +1,21 @@
 export default function() {
+  var oldcheckPassthrough = this.pretender.checkPassthrough;
+  this.pretender.checkPassthrough = function (request) {
+    request.url = parseURL(request.url).fullpath;
+    return oldcheckPassthrough.apply(this, [request]);
+  };
+
+  this.namespace = '/v2';
+
+
+  this.get('/channels/:id', ['channel', 'episodes']);
+}
+
+function parseURL(url) {
+  var anchor = document.createElement('a');
+  anchor.href = url;
+  anchor.fullpath = anchor.pathname + (anchor.search || '') + (anchor.hash || '');
+  return anchor;
 }
 
 /*
