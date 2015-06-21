c53a16bca1a8cbf5d7e5f2b0fc90f21e6bcaeb2b
Duke <dukekhaos@gmail.com>
2015-06-21 18:57:24 -0300

    add mirage factories #59

---
+++ app/mirage/factories/channel.js
@@ -0,0 +1,7 @@
+import Mirage from 'ember-cli-mirage';
+
+export default Mirage.Factory.extend({
+  title: i => `Channel ${i}`,
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image_url: i => `http://lorempixel.com/400/400/?${i}`
+});
