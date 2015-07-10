1773f4e3829c12210d1089e50c81699dea204955
Duke <dukekhaos@gmail.com>
2015-07-10 06:02:43 -0300

    using ember-i18n

--- app/initializers/i18n.js
+++ app/initializers/i18n.js
@@ -1,11 +1,6 @@
-import Ember from 'ember';
-import i18n from 'i18n';
+// import Ember from 'ember';
 
-export function initialize( container, application ) {
-  Ember.Handlebars.registerBoundHelper('t', i18n.t);
-
-  application.register('service:i18n', i18n, {instantiate: false});
-  application.inject('controller', 'i18n', 'service:i18n');
+export function initialize( /* container, application */ ) {
 }
 
 export default {
