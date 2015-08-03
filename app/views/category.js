a350caf0580a1e55a3b1bec3b34658d51d56c07d
Duke <dukekhaos@gmail.com>
2015-08-02 23:08:56 -0300

    remove more code

--- app/views/category.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-import LazyImageViewMixin from '../mixins/lazy-image-view-mixin';
-
-export default Ember.View.extend(LazyImageViewMixin, {
-});
--- app/views/users/settings.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.View.extend({
-  __closeMenu: function () {
-    this.container.lookup('controller:application').send('closeMenu');
-  }.on('didInsertElement')
-});
