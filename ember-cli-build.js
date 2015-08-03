bb1db455ff9d773cf3f32b1bbfa3c99caddb3330
Duke <dukekhaos@gmail.com>
2015-08-02 23:15:58 -0300

    import moment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,10 +3,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
   });
 
   app.import('bower_components/material-design-lite/material.js');
+  app.import('bower_components/moment/moment.js');
 
   return app.toTree();
 };
