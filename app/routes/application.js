023952753b756dd608c1ecf526aa2944011a574b
Duke <dukekhaos@gmail.com>
2015-08-17 09:03:11 -0300

    upgrade mdl elements on channel route

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,4 +2,12 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  setupController (a,b) {
+    Ember.run.scheduleOnce('afterRender', this, function () {
+      window.componentHandler.upgradeAllRegistered();
+    });
+
+    return this._super(a, b);
+  }
+});
