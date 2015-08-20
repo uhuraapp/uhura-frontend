35c31b43662bafe75bc8a256c0c7e63c19b012b1
Duke <dukekhaos@gmail.com>
2015-08-20 09:36:08 -0300

    start player style

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,12 +5,13 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
   });
 
-  app.import("bower_components/mediaelement/build/mediaelementplayer.css");
+  app.import('bower_components/mediaelement/build/mediaelementplayer.css');
 
   app.import('bower_components/material-design-lite/material.js');
   app.import('bower_components/moment/moment.js');
-  app.import("bower_components/mediaelement/build/mediaelement-and-player.js");
+  app.import('bower_components/mediaelement/build/mediaelement-and-player.js');
 
+  app.import('bower_components/mediaelement/build/controls.svg', { destDir: 'assets' });
 
   return app.toTree();
 };
