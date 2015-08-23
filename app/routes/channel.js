5dc9675ed59ce7c79997bf592d745800972b0483
Duke <dukekhaos@gmail.com>
2015-08-23 00:35:00 -0300

    add page-title addon

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -5,17 +5,17 @@ export default Ember.Route.extend({
     return this.store.find('channel', params.channel_id);
   },
   setupController(controller, model) {
-    if (model.get('episodes').length === 0) {
-      model.reload();
-    }
-
+    // if (model.get('episodes').length === 0) {
+    //   model.reload();
+    // }
+    //
     this._super(controller, model);
-
-    let title = model.get('title');
-
-    Ember.$("meta[name='description']").attr('value', model.get('description'));
-    Ember.$("[property='og:image']").attr('content', model.get('image_url'));
-    Ember.$("[property='og:title']").attr('content', title);
-    Ember.$(document).attr('title', title);
+    //
+    // let title = model.get('title');
+    //
+    // Ember.$("meta[name='description']").attr('value', model.get('description'));
+    // Ember.$("[property='og:image']").attr('content', model.get('image_url'));
+    // Ember.$("[property='og:title']").attr('content', title);
+    // Ember.$(document).attr('title', title);
   }
 });
