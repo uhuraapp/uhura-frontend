ffc6ea5ac03165ca2f92cc47614ff9a2e40f7b87
Duke <dukekhaos@gmail.com>
2015-08-24 01:31:31 -0300

    add material design mixin to register mdl components

---
+++ app/mixins/routes/material-design.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  setupController(a, b) {
+    Ember.run.scheduleOnce('afterRender', this, 'upgradeElements');
+    this._super(a, b);
+  },
+  upgradeElements() {
+    let elements = document.querySelectorAll('.page-content [class^=mdl]');
+    [].forEach.call(elements, el => componentHandler.upgradeElement(el));
+  }
+});
---
+++ tests/unit/mixins/routes/material-design-test.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import RoutesMaterialDesignMixin from '../../../mixins/routes/material-design';
+import { module, test } from 'qunit';
+
+module('Unit | Mixin | routes/material design');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let RoutesMaterialDesignObject = Ember.Object.extend(RoutesMaterialDesignMixin);
+  let subject = RoutesMaterialDesignObject.create();
+  assert.ok(subject);
+});
