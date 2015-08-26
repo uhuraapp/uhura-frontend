4cc9209a67a2ab0ab2431d1232f27d91ad1573c9
Duke <dukekhaos@gmail.com>
2015-08-26 16:53:58 -0300

    fix infinity scroll on episode list

--- app/components/episodes-list.js
+++ app/components/episodes-list.js
@@ -19,11 +19,20 @@ export default Ember.Component.extend({
         let since = moment.utc(date).format();
 
         // TODO: use meta
-        promise = this.get('store').find('episode', {
+        promise = this.get('store').query('episode', {
           since,
           channel_id: this.get('channel.raw_id'),
           per_page:   20
+        }).then(function(response) {
+          // Fix error with _internalModel
+          let items = []
+          response.get('content').forEach(function(internalModel) {
+            items.push({_internalModel: internalModel});
+          });
+          response.set('content', items);
+          return response;
         });
+
       }
 
       callback(promise);
