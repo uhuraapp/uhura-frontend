19d475b96e50e1eb29e0fddd81f8b7afe087850e
Duke <dukekhaos@gmail.com>
2015-08-29 20:27:09 -0300

    use material design in channel route

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -1,21 +1,8 @@
 import Ember from 'ember';
+import MaterialDesignMixin from '../mixins/routes/material-design';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(MaterialDesignMixin, {
   model(params) {
     return this.store.find('channel', params.channel_id);
-  },
-  setupController(controller, model) {
-    // if (model.get('episodes').length === 0) {
-    //   model.reload();
-    // }
-    //
-    this._super(controller, model);
-    //
-    // let title = model.get('title');
-    //
-    // Ember.$("meta[name='description']").attr('value', model.get('description'));
-    // Ember.$("[property='og:image']").attr('content', model.get('image_url'));
-    // Ember.$("[property='og:title']").attr('content', title);
-    // Ember.$(document).attr('title', title);
   }
 });
