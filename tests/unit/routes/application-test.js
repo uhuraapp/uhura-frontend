0e45a67a118ecb1b50f09bf5d3f7e321a8d5a61e
Duke <dukekhaos@gmail.com>
2015-08-29 22:10:40 -0300

    mock service:session on application route test

--- tests/unit/routes/application-test.js
+++ tests/unit/routes/application-test.js
@@ -3,8 +3,22 @@ import {
   test
 } from 'ember-qunit';
 
+import Ember from 'ember';
+
+const { Evented } = Ember;
+
+const session = Ember.Object.extend(Evented, {
+  content: {}
+}).create();
+
+const sessionService = Ember.Object.extend(Evented);
+
 moduleFor('route:application', {
-  needs: ['service:session']
+  beforeEach() {
+    const { container } = this;
+    const service = sessionService.create({ container, session });
+    this.registry.register('service:session', service, { instantiate: false });
+  }
 });
 
 test('it exists', function(assert) {
