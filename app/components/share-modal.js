f8e7bb5d1766f547d7836cf3655d2182b0be55c0
Duke <dukekhaos@gmail.com>
2015-09-01 01:23:01 -0300

    fix close modal button, #119

--- app/components/share-modal.js
+++ app/components/share-modal.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   currentURL: Ember.computed(() => {
     return `${window.location.toString().split('?')[0]}?utm_source=uhura+modal&utm_medium=site`;
-  })
+  }),
+
+  actions: {
+    close() {
+      this.sendAction(this.attrs['dismiss']);
+    }
+  }
 });
