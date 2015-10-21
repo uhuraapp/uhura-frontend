4bdc11a31fef6aa48897d8dea162e4622f5228e4
Duke <dukekhaos@gmail.com>
2015-10-21 10:22:40 -0200

    notify user about profile update #17

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2,8 +2,14 @@ import Ember from 'ember';
 import MaterialDesignMixin from '../mixins/routes/material-design';
 import TitledMixin from '../mixins/routes/titled';
 
-export default Ember.Route.extend(MaterialDesignMixin, TitledMixin, {
-  client: Ember.inject.service('uhura-client'),
+
+const { Route, inject } = Ember;
+
+
+export default Route.extend(MaterialDesignMixin, TitledMixin, {
+  client: inject.service('uhura-client'),
+  notify: inject.service('notify'),
+
   actions: {
     save() {
       const name = this.get('controller.name');
@@ -11,6 +17,7 @@ export default Ember.Route.extend(MaterialDesignMixin, TitledMixin, {
       const user = { name, locale };
       const data = { user };
       this.get('client').request('', 'user', '', 'PUT', { data }).then((user) => {
+        this.get('notify').success('Profile updated!');
         this.container.lookup('session:main')._setup('authenticator:uhura', user, true);
       });
     }
