8b6e59a6935782ad091784e8ed7017e020d12dcb
Duke <dukekhaos@gmail.com>
2015-11-02 17:03:58 -0200

    update channel title

--- app/routes/channel.js
+++ app/routes/channel.js
@@ -2,14 +2,20 @@ import Ember from 'ember';
 import MaterialDesignMixin from '../mixins/routes/material-design';
 import TitledMixin from '../mixins/routes/titled';
 
-const { service } = Ember.inject;
+const { inject: { service }, computed } = Ember;
 
 export default Ember.Route.extend(MaterialDesignMixin, TitledMixin, {
   session: service('session'),
+
+  shouldSetTitle: computed('session.isAuthenticated', function() {
+    return this.get('session.isAuthenticated');
+  }),
+
   beforeModel(transition) {
     this.get('session').set('attemptedTransition', transition);
     return this._super(transition);
   },
+
   model(params) {
     return this.store.find('channel', params.channel_id);
   }
