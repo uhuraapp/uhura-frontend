d2dc2ec058c4d444df3a24a642f3cfd1dea297ae
Duke <dukekhaos@gmail.com>
2015-11-02 16:39:41 -0200

    add way to disable title

--- app/mixins/routes/titled.js
+++ app/mixins/routes/titled.js
@@ -1,13 +1,21 @@
 import Ember from 'ember';
 
+const { computed, run } = Ember;
+
 export default Ember.Mixin.create({
   setupController(a, b) {
     this._super(a, b);
-    Ember.run.scheduleOnce('afterRender', this, 'setPageTitle');
+    run.scheduleOnce('afterRender', this, 'setPageTitle');
   },
 
+  shouldSetTitle: computed(() => true),
+
   setPageTitle() {
-    const title = this.get('controller.title') || this.get('controller.model.title');
+    let title = '';
+    if (this.get('shouldSetTitle')) {
+      title = this.get('controller.title') || this.get('controller.model.title');
+    }
+
     this.send('setTitle', title);
   }
 });
