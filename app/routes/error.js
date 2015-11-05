326c9d5d5298449439edb1fef4ad2e7f853350dc
Duke <dukekhaos@gmail.com>
2015-11-05 00:13:59 -0200

    add error page #157

---
+++ app/routes/error.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import TitledMixin from '../mixins/routes/titled';
+
+const { set, computed } = Ember;
+
+export default Ember.Route.extend(TitledMixin, {
+  shouldSetTitle: computed(() => false),
+  setupController(controller, adapterError) {
+    const { title, details } = this.__error(adapterError.errors);
+    set(controller, 'title', title);
+    set(controller, 'details', `For some reason we can not complete your request,
+        We are receiving the follow error: <strong>${details}</strong>.</br /><br />
+        We were notified of the problem and soon we will fix it, <strong>try reloading the page</strong>, if the problem persists please email uhura@support.io`);
+    this._super(controller, adapterError);
+  },
+
+  __error(errors) {
+    let title = 'We\'re sorry';
+    let details = '';
+
+    if (errors.length === 1) {
+      const [ error ] = errors;
+      details = error.title;
+    }
+
+    return { title, details };
+  }
+});
