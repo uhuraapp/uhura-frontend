fe0dcf951180472d4822d5101e740aa0c030f845
Duke <dukekhaos@gmail.com>
2016-01-03 14:12:54 -0200

    add initial unresponsive channel search

--- app/routes/subscriptions/new.js
+++ app/routes/subscriptions/new.js
@@ -1,11 +1,39 @@
 import Ember from 'ember';
 
+const { isEmpty, RSVP: { Promise } } = Ember;
+
 export default Ember.Route.extend({
-  model() {
+  client: Ember.inject.service('uhura-client'),
+
+  queryParams: {
+    q: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
     return Ember.RSVP.hash({
       categories: this.store.findAll('category')
         .then((data) => data.sortBy('channelsLength'))
-        .then((data) => data.reverse())
+        .then((data) => data.reverse()),
+      channels: this.searchChannel(params.q)
     });
+  },
+
+  searchChannel(q) {
+    if (isEmpty(q)) {
+      return Promise.resolve([]);
+    }
+
+    const data = { q };
+    return this.get('client').request('channels', null, null, 'GET', { data })
+      .then((data) => data.channels);
+  },
+
+  actions: {
+    search() {
+      const q = this.get('controller.termSearch');
+      this.searchChannel(q).then((channels) => this.get('controller.model.channels', channels));
+    }
   }
 });
