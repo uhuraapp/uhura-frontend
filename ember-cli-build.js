63185e4e2122c0dc4baaf0800fd3da9365d4d0bd
Duke <dukekhaos@gmail.com>
2016-01-10 01:28:05 -0200

    remove channel-placeholder fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    fingerprint: {
+      exclude: ['channel-placeholder.png']
+    },
     sassOptions: {
       extentions: 'sass'
     }
