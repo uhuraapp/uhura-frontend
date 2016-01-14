9a9b667f89ff189dbf99d5e92b7134723049713b
Duke <dukekhaos@gmail.com>
2016-01-14 18:11:27 -0200

    use Ahoy ApplicationRouteMixin to track views

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import AhoyApplicationRouteMiin from 'ember-ahoy/mixins/application-route-mixin'
 
 const { service } = Ember.inject;
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+export default Ember.Route.extend(ApplicationRouteMixin, AhoyApplicationRouteMiin, {
   session: service('session'),
   actions: {
     // TODO: remove it
